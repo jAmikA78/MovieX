df44592b4babf22a1f231952a8336940dae9f846
Ahmed-Alktatney <ahmedalktatny5@gmail.com>
2026-03-23 18:41:55 +0200

    Update MainActivity.kt
    Preparing the program's login interface, which consists of four parts.

--- app/src/main/java/com/depi/moviex/MainActivity.kt
+++ app/src/main/java/com/depi/moviex/MainActivity.kt
@@ -1,17 +1,18 @@
-package com.depi.moviex
+package com.example.moviex
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.depi.moviex.ui.theme.MovieXTheme
+import androidx.compose.ui.graphics.Color
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.moviex.ui.screens.SplashScreen
+import com.example.moviex.ui.screens.onboarding.OnboardingScreen
+import com.example.moviex.ui.theme.MovieXTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,29 +20,36 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             MovieXTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                AppNavigation()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name ",
-        modifier = modifier
-    )
-}
+fun AppNavigation() {
+    val navController = rememberNavController()
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MovieXTheme {
-        Greeting("Android")
+    NavHost(navController = navController, startDestination = "splash") {
+
+        composable("splash") {
+            SplashScreen(onTimeout = {
+                navController.navigate("onboarding") {
+                    popUpTo("splash") { inclusive = true }
+                }
+            })
+        }
+
+        composable("onboarding") {
+            OnboardingScreen(onFinish = {
+                navController.navigate("home") {
+                    popUpTo("onboarding") { inclusive = true }
+                }
+            })
+        }
+
+        composable("home") {
+            Text(text = "Welcome to MovieX", color = Color.Blue)
+        }
     }
 }
